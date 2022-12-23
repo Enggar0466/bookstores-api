7ff87c002c687481276b753bc66dfa4808d7e38e
Enggar0466 <Enggar0466@gmail.com>
2022-12-24 00:02:31 +0700

    change code inspired by gilang pointofsale

--- app.js
+++ app.js
@@ -1,22 +1,72 @@
-require('dotenv').config();
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+  }); // Config environment
+  const express = require("express"); // Import expres
+//   const cors = require("cors");
+  const morgan = require("morgan");
+  const app = express(); // Make express app
+  
+  
+    app.use(morgan("dev"));
+  
+  
+  /* Import routes */
+  const books = require("./routes/books");
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+  /* Import response */
+  const response = require("./middlewares/response");
+  
+  /* Enable req.body */
+  app.use(express.json()); // Enable req.body JSON
+  // Enable url-encoded
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  
+  /* Make public folder as static */
+  app.use(express.static("public"));
+  
+  /* Use routes */
+  app.get("/", async (req, res, next) => {
+    try {
+      res.redirect("https://documenter.getpostman.com/view/14563768/TzzGFt9w");
+    } catch (error) {
+      next(error);
+    }
+  });
+  
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+  /* The routes */
+  const version = "/api/v1";
+  
+  app.use(`${version}/books`, books);
 
-var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+  
+  /* If routes not
+  found */
+  app.all("*", (req, res, next) => {
+    try {
+      next({
+        message: "Endpoint not Found",
+        error: "Not Found",
+        statusCode: 404,
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  /* User response */
+  app.use(response);
+  
+  /* Running server */
+  const PORT = process.env.PORT || 3000;
+  if (process.env.NODE_ENV !== "test") {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  }
+  
+  module.exports = app;
+  
